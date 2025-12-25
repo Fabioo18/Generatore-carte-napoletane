90fa424361fc3e253b83f6ea115928840689e25c
Fabio Lacarbonara <lacarbonarafabio18@gmail.com>
2025-12-25 20:00:17 +0100

    Creazione app PWS

---
+++ service-worker.js
@@ -0,0 +1,128 @@
+const CACHE_NAME = "carte-napoletane-v2"; // Incrementa la versione quando aggiorni i file
+
+// Lista di tutti i file da mettere in cache
+const urlsToCache = [
+  "/index.html",
+  "/static/sfondo.jpg",
+  "/static/Carte_Napoletane_retro.jpg",
+  // Immagini carte
+  "/static/01_Asso_di_denari.jpg",
+  "/static/02_Due_di_denari.jpg",
+  "/static/03_Tre_di_denari.jpg",
+  "/static/04_Quattro_di_denari.jpg",
+  "/static/05_Cinque_di_denari.jpg",
+  "/static/06_Sei_di_denari.jpg",
+  "/static/07_Sette_di_denari.jpg",
+  "/static/08_Otto_di_denari.jpg",
+  "/static/09_Nove_di_denari.jpg",
+  "/static/10_Dieci_di_denari.jpg",
+  "/static/11_Asso_di_coppe.jpg",
+  "/static/12_Due_di_coppe.jpg",
+  "/static/13_Tre_di_coppe.jpg",
+  "/static/14_Quattro_di_coppe.jpg",
+  "/static/15_Cinque_di_coppe.jpg",
+  "/static/16_Sei_di_coppe.jpg",
+  "/static/17_Sette_di_coppe.jpg",
+  "/static/18_Otto_di_coppe.jpg",
+  "/static/19_Nove_di_coppe.jpg",
+  "/static/20_Dieci_di_coppe.jpg",
+  "/static/21_Asso_di_spade.jpg",
+  "/static/22_Due_di_spade.jpg",
+  "/static/23_Tre_di_spade.jpg",
+  "/static/24_Quattro_di_spade.jpg",
+  "/static/25_Cinque_di_spade.jpg",
+  "/static/26_Sei_di_spade.jpg",
+  "/static/27_Sette_di_spade.jpg",
+  "/static/28_Otto_di_spade.jpg",
+  "/static/29_Nove_di_spade.jpg",
+  "/static/30_Dieci_di_spade.jpg",
+  "/static/31_Asso_di_bastoni.jpg",
+  "/static/32_Due_di_bastoni.jpg",
+  "/static/33_Tre_di_bastoni.jpg",
+  "/static/34_Quattro_di_bastoni.jpg",
+  "/static/35_Cinque_di_bastoni.jpg",
+  "/static/36_Sei_di_bastoni.jpg",
+  "/static/37_Sette_di_bastoni.jpg",
+  "/static/38_Otto_di_bastoni.jpg",
+  "/static/39_Nove_di_bastoni.jpg",
+  "/static/40_Dieci_di_bastoni.jpg",
+  // Audio
+  "/static/Asso_di_Denari.mp3",
+  "/static/Due_di_Denari.mp3",
+  "/static/Tre_di_Denari.mp3",
+  "/static/Quattro_di_Denari.mp3",
+  "/static/Cinque_di_Denari.mp3",
+  "/static/Sei_di_Denari.mp3",
+  "/static/Sette_di_Denari.mp3",
+  "/static/Otto_di_Denari.mp3",
+  "/static/Nove_di_Denari.mp3",
+  "/static/Dieci_di_Denari.mp3",
+  "/static/Asso_di_Coppe.mp3",
+  "/static/Due_di_Coppe.mp3",
+  "/static/Tre_di_Coppe.mp3",
+  "/static/Quattro_di_Coppe.mp3",
+  "/static/Cinque_di_Coppe.mp3",
+  "/static/Sei_di_Coppe.mp3",
+  "/static/Sette_di_Coppe.mp3",
+  "/static/Otto_di_Coppe.mp3",
+  "/static/Nove_di_Coppe.mp3",
+  "/static/Dieci_di_Coppe.mp3",
+  "/static/Asso_di_Spade.mp3",
+  "/static/Due_di_Spade.mp3",
+  "/static/Tre_di_Spade.mp3",
+  "/static/Quattro_di_Spade.mp3",
+  "/static/Cinque_di_Spade.mp3",
+  "/static/Sei_di_Spade.mp3",
+  "/static/Sette_di_Spade.mp3",
+  "/static/Otto_di_Spade.mp3",
+  "/static/Nove_di_Spade.mp3",
+  "/static/Dieci_di_Spade.mp3",
+  "/static/Asso_di_Bastoni.mp3",
+  "/static/Due_di_Bastoni.mp3",
+  "/static/Tre_di_Bastoni.mp3",
+  "/static/Quattro_di_Bastoni.mp3",
+  "/static/Cinque_di_Bastoni.mp3",
+  "/static/Sei_di_Bastoni.mp3",
+  "/static/Sette_di_Bastoni.mp3",
+  "/static/Otto_di_Bastoni.mp3",
+  "/static/Nove_di_Bastoni.mp3",
+  "/static/Dieci_di_Bastoni.mp3"
+];
+
+// Installazione: cache dei file
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch: serve dalla cache, fallback a index.html se offline
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request).catch(() => {
+        // fallback solo per navigazione pagina
+        if (event.request.mode === 'navigate') {
+          return caches.match('/index.html');
+        }
+      });
+    })
+  );
+});
+
+// Attivazione: pulizia vecchie cache
+self.addEventListener("activate", event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (!cacheWhitelist.includes(key)) return caches.delete(key);
+        })
+      )
+    )
+  );
+});
